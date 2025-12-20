8807148b8097dc19fb0921b51f4df7865433e68e
Kuan <roumiankuan@outlook.com>
2025-12-20 19:54:29 +0800

    87,模板语法

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,47 +1,21 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <!--这是一个注释，哈哈哈-->
+  <h3>模板语法</h3>
+  <p>{{ msg }}</p>
+  <p>{{ hello }}</p>
+  <p>{{ rawHtml }}</p>
+  <!--原始html-->
+  <p v-html="rawHtml"></p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script lang="ts">
+export default {
+  data() {
+    return {
+      msg: '神奇的语法',
+      hello: 'hello world',
+      rawHtml: "<a href='https://baidu.com'>百战程序员</a>",
+    }
+  },
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+</script>
