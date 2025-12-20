f36855ac437eb8045e2d3e9ff1dd9e4d485add04
Kuan <roumiankuan@outlook.com>
2025-12-20 22:05:40 +0800

    列表渲染v-for :key

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <HelloWorld msg=" You did it!" />
   <IfDemo />
+  <ListDemo />
 </template>
 
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import IfDemo from './components/IfDemo.vue'
+import ListDemo from './components/ListDemo.vue'
 </script>
---
+++ vue-project/src/components/ListDemo.vue
@@ -0,0 +1,17 @@
+<template>
+  <h3>列表渲染</h3>
+  <p v-for="item in names" :key="item">{{ item }}</p>
+  <p v-for="item of names" :key="item">{{ item }}</p>
+  <p v-for="(item, index) in names" :key="item">{{ item }}-{{ index }}</p>
+  <div>
+    <p v-for="value in userInfo" :key="value">{{ value }}</p>
+    <p v-for="(value, key, index) in userInfo" :key="value">{{ key }}--{{ value }}--{{ index }}</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const names = ref(['百战程序员', '尚学堂', 'IT'])
+const userInfo = ref({ name: 'iwen', age: 20, sex: 'man' })
+</script>
