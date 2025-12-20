3bd434f704f60f3f8d42093c0bfe7da59c0a52ad
Kuan <roumiankuan@outlook.com>
2025-12-20 20:54:24 +0800

    88，属性绑定

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,21 +1,7 @@
 <template>
-  <!--这是一个注释，哈哈哈-->
-  <h3>模板语法</h3>
-  <p>{{ msg }}</p>
-  <p>{{ hello }}</p>
-  <p>{{ rawHtml }}</p>
-  <!--原始html-->
-  <p v-html="rawHtml"></p>
+  <HelloWorld msg=" You did it!" />
 </template>
 
-<script lang="ts">
-export default {
-  data() {
-    return {
-      msg: '神奇的语法',
-      hello: 'hello world',
-      rawHtml: "<a href='https://baidu.com'>百战程序员</a>",
-    }
-  },
-}
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
 </script>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-bind:id="dynamicId" :class="dynamicClass" v-bind:title="dynamicTitle">测试</div>
+  <button :disabled="isButtonDisabled">Button</button>
+  <div v-bind="objectofAttrs">编译</div>
+</template>
+
+<script lang="ts">
+export default {
+  data() {
+    return {
+      dynamicClass: 'appClass',
+      dynamicId: 'appId',
+      dynamicTitle: undefined,
+      isButtonDisabled: false,
+      objectofAttrs: {
+        id: 'appid',
+        class: 'appclass',
+      },
+    }
+  },
+}
+</script>
+
+<style>
+.appClass {
+  color: red;
+  font-size: 20px;
+}
+</style>
