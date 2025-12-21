ce8a1ee17618044726e6b33843d7eb86cde97dc4
Kuan <roumiankuan@outlook.com>
2025-12-21 20:49:30 +0800

    106, 组件props传递多种数据类型

--- vue-components/src/components/ChildDemo.vue
+++ vue-components/src/components/ChildDemo.vue
@@ -1,10 +1,22 @@
 <template>
   <h3>Child</h3>
   <p>{{ props.title }}</p>
-  <p>{{ props.demo }}</p>
+  <p>{{ props.age }}</p>
+  <ul>
+    <li v-for="(item, index) in props.names" :key="index">{{ item }}</li>
+  </ul>
+  <p>{{ userInfo.name }}</p>
+  <p>{{ userInfo.age }}</p>
 </template>
 
 <script setup lang="ts">
 // defineProps用来定义一个对象，该对象可以从父类接收数据
-const props = defineProps(['title', 'demo'])
+const props = defineProps<{
+  title: string // 字符串类型
+  age: number // 数字类型
+  names: string[] // 字符串数组类型（方案1传递时解包的情况）
+  userInfo: { name: string; age: number }
+}>()
+
+console.log(props)
 </script>
--- vue-components/src/components/ParentDemo.vue
+++ vue-components/src/components/ParentDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <h3>Parent</h3>
-  <ChildDemo :title="message" demo="测试" />
+  <ChildDemo :title="message" :age="age" :names="names" :userInfo="userInfo" />
 </template>
 
 <script setup lang="ts">
@@ -8,6 +8,12 @@ import { ref } from 'vue'
 import ChildDemo from './ChildDemo.vue'
 
 const message = ref('Parent数据!')
+
+const age = ref(20)
+
+const names = ref(['iwen', 'ime', 'frank'])
+
+const userInfo = ref({ name: 'iwen', age: 20 })
 
 // props传递数据，只能从父级传递到子级，不能反向传递
 </script>
