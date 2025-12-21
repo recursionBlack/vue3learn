f2811dd26fc0fd85792bea5411d4835374ee628a
Kuan <roumiankuan@outlook.com>
2025-12-21 19:49:24 +0800

    104，组件的全局注册

--- vue-components/src/App.vue
+++ vue-components/src/App.vue
@@ -1,11 +1,11 @@
 <template>
-  <HeaderOfApp />3
+  <HeaderOfApp />
   <MainOfApp />
   <AsideOfApp />
 </template>
 
 <script setup lang="ts">
-import HeaderOfApp from './pages/HeaderOfApp.vue'
+// import HeaderOfApp from './pages/HeaderOfApp.vue'
 import MainOfApp from './pages/MainOfApp.vue'
 import AsideOfApp from './pages/AsideOfApp.vue'
 </script>
--- vue-components/src/main.ts
+++ vue-components/src/main.ts
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import HeaderOfApp from './pages/HeaderOfApp.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// 在这中间写组件的注册
+// 全局注册
+app.component('HeaderOfApp', HeaderOfApp)
+
+app.mount('#app')
