c8e9cca260d6fd9c10d13185d462ae00af453c80
Kuan <roumiankuan@outlook.com>
2025-12-21 09:43:14 +0800

    91，列表渲染中的key使用

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,10 +2,12 @@
   <HelloWorld msg=" You did it!" />
   <IfDemo />
   <ListDemo />
+  <KeyDemo />
 </template>
 
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import IfDemo from './components/IfDemo.vue'
 import ListDemo from './components/ListDemo.vue'
+import KeyDemo from './components/KeyDemo.vue'
 </script>
---
+++ vue-project/src/components/KeyDemo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h3>Key属性添加到v-for中</h3>
+  <p v-for="(item, index) in names" :key="index">{{ item }}</p>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const names = ref(['百战程序员', '尚学堂', 'IT'])
+
+// 为了给vue一个提示，以便它追踪每个节点的标识，从而重用和重新排序现有的元素（而不是因为位置调整导致的重新渲染）
+// 你需要为每个元素对应的块，提供一个唯一的key attribute：
+</script>
